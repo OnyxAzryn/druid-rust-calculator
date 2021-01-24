44e64c2c28f2c385600341e2d18976871fc05717
Onyx Azryn <brycekabat@onyxazryn.com>
2021-01-24 11:38:49 -0600

    Renamed

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-// Use RUI module
-mod rui;
+// Use calculator module
+mod druid_rust_calculator;
 
 // External crates
 use druid::{AppLauncher, LocalizedString, WindowDesc};
 use rug::Float;
 
 pub fn main() {
-    let window = WindowDesc::new(rui::build_calc)
+    let window = WindowDesc::new(druid_rust_calculator::build_calc)
         .window_size((223., 300.))
         .resizable(false)
         .title(
             LocalizedString::new("calc-demo-window-title").with_placeholder("Simple Calculator"),
         );
-    let calc_state = rui::calc_state::CalcState {
+    let calc_state = druid_rust_calculator::calc_state::CalcState {
         value: "0".to_string(),
-        operand: rui::druid_float::DruidFloat {
+        operand: druid_rust_calculator::druid_float::DruidFloat {
             value: Float::new(16),
         },
         operator: 'C',
